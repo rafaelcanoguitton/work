8da6fbdcb938716ffe3428bcfde5f8d524ca482a
UCSP <UCSP@UCSP.AULAB>
2017-03-24 11:24:52 -0500

    rofl

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    float x,y;
+    cout << "Ingrese su x: "<<endl;
+    cin >> x ;
+    cout << "Ingrese su y: "<<endl;
+    cin >> y;
+    if (x<y)
+        cout<<y<<endl;
+    else;
+        cout<<x<<endl;
+    /*
+    float x;
+    float y;
+    cout << "Ingrese su x: "<<endl;
+    cin >> x ;
+    cout << "Ingrese su y: "<<endl;
+    cin >> y ;
+    cout << "Pum pururun magia :v"<<endl;
+    x=x+y;
+    y=x-y;
+    x=x-y;
+    cout<< "Valor de x:"<<x<<endl;
+    cout<< "Valor de y:"<<y<<endl;
+
+    float x;
+    float y;
+    float z;
+    cout << "Ingrese su x: "<<endl;
+    cin >> x ;
+    cout << "Ingrese su y: "<<endl;
+    cin >> y ;
+    cout << "Pum pururun magia :v"<<endl;
+    z=x ;
+    x=y ;
+    y=z ;
+    cout << "Sus variable son:"<< x <<" "<< y<<endl;
+
+
+    float x ;
+    float l ;
+    float r ;
+    cout << "Hallar el area del circulo:" << endl;
+    cout << "Ingrese el radio:" << endl;
+    cin >> x ;
+    r= x*3.14159265359 ;
+    l= r*r ;
+    cout <<"Su resultado es: "<< r << endl;
+    float a ;
+    float b ;
+    cout << "Hallar el area del cuadrado:" << endl;
+    cout << "Ingrese el lado:" << endl;
+    cin >> a ;
+    b= a*a ;
+    cout <<"Su resultado es: "<< b << endl;
+    float c ;
+    float d ;
+    float e ;
+    cout << "Hallar el area del rectangulo:" << endl;
+    cout << "Ingrese su base:" << endl;
+    cin >> c ;
+    cout << "Ingrese su altura::" << endl;
+    cin >> d ;
+    e= d*c ;
+    cout <<"Su resultado es: "<< e << endl;*/
+    return 0;
+}
+
